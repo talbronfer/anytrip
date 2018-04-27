983bb0d4b388907e0d46e0ba607c8ee68fa5f445
Tal Bronfer <tal.bronfer@safebreach.com>
2018-04-27 09:26:01 +0300

    deploy shit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const http = require('http');
 const bodyParser = require('body-parser');
 
 // Get our API routes
-const api = require('./server/routes/api');
+// const api = require('./server/routes/api');
 
 const app = express();
 
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Set our api routes
-app.use('/api', api);
+// app.use('/api', api);
 
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,51 +1,51 @@
-import { Session } from 'protractor';
-
-const express = require('express');
-const router = express.Router();
-var mongoose = require('mongoose');
-var moment = require('moment');
-
-mongoose.connect("mongodb://localhost:27017/anytrip");
-
-var tripSchema = require('../models/trip');
-var userSchema = require('../models/user');
-
-var Trip = mongoose.model('Trip', tripSchema);
-var User = mongoose.model('User', userSchema);
 
 
-/* GET api listing. */
-router.get('/trips', (req, res) => {
-  var query = null;
-  if (req.query.uid) {
-    query = Trip.find({ user_id: req.query.uid });
-  }
+// const express = require('express');
+// const router = express.Router();
+// var mongoose = require('mongoose');
+// var moment = require('moment');
 
-  else {
-    query = Trip.find();
-  }
+// mongoose.connect("mongodb://localhost:27017/anytrip");
 
-  query.exec().then((result, err) => {
-    res.send(result);
-  })
-});
+// var tripSchema = require('../models/trip');
+// var userSchema = require('../models/user');
 
-router.get('/trips/:id', (req, res) => {
-  Trip.findById(req.params.id, (err, result) => {
-    res.send(result);
-  })
-})
+// var Trip = mongoose.model('Trip', tripSchema);
+// var User = mongoose.model('User', userSchema);
 
-router.post('/trips', (req, res) => {
-  var trip = new Trip(req.body);
-  trip.save();
-  res.send(trip);
-});
 
-router.post('/sessions', (req, res) => {
-  var session = new Session(req.body);
-  session.save();
-  res.send(session);
-});
+// /* GET api listing. */
+// router.get('/trips', (req, res) => {
+//   var query = null;
+//   if (req.query.uid) {
+//     query = Trip.find({ user_id: req.query.uid });
+//   }
 
-module.exports = router;
+//   else {
+//     query = Trip.find();
+//   }
+
+//   query.exec().then((result, err) => {
+//     res.send(result);
+//   })
+// });
+
+// router.get('/trips/:id', (req, res) => {
+//   Trip.findById(req.params.id, (err, result) => {
+//     res.send(result);
+//   })
+// })
+
+// router.post('/trips', (req, res) => {
+//   var trip = new Trip(req.body);
+//   trip.save();
+//   res.send(trip);
+// });
+
+// router.post('/sessions', (req, res) => {
+//   var session = new Session(req.body);
+//   session.save();
+//   res.send(session);
+// });
+
+// module.exports = router;
