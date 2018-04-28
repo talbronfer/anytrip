08f44564f29aa3dc92ee2568103661c5f29def3a
Tal Bronfer <tal.bronfer@safebreach.com>
2018-04-28 21:54:44 +0300

    ui touchups for demo:wq

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase, AngularFireObject, AngularFireList } from 'angularfire2/database';
 import { UserService } from '../user.service';
-import {Router} from '@angular/router';
+import {Router, ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -13,10 +13,18 @@ export class LoginComponent implements OnInit {
   public sessions :  AngularFireList<any>;
   public email : string;
 
-  constructor(private db: AngularFireDatabase, private userService : UserService, private router : Router) { }
+  constructor(private db: AngularFireDatabase,
+     private userService : UserService,
+      private router : Router,
+      private activatedRoute: ActivatedRoute)
+       { }
 
   ngOnInit() {
     this.sessions = this.db.list('/sessions');
+    this.activatedRoute.queryParams.subscribe((params) => {
+      let demoMode = params.demoMode;
+      this.userService.demoMode = demoMode;
+    })
   }
 
   start() {
